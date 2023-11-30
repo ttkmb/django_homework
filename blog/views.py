780ce3f51c6eabdedc65360711d18320e1b55730
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2023-11-30 20:53:53 +0300

    modified output and query processing

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Post
 
 
 class PostListView(ListView):
     model = Post
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class PostCreateView(CreateView):
@@ -18,9 +24,22 @@ class PostCreateView(CreateView):
         context['title'] = 'Добавление нового поста'
         return context
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+        return super().form_valid(form)
+
 
 class PostDetailView(DetailView):
     model = Post
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.count_views += 1
+        self.object.save()
+        return self.object
 
 
 class PostUpdateView(UpdateView):
@@ -38,6 +57,9 @@ class PostUpdateView(UpdateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Обновление поста'
         return context
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post', kwargs={'pk': self.object.pk})
 
 
 class PostDeleteView(DeleteView):
